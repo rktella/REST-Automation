6b2796e1efb4b9ece7b0651ec9d551cd6c0bafff
Ravi Tella <Ravi Tella>
2017-03-24 12:46:44 -0400

    add project

---
+++ REST Batch.bat
@@ -0,0 +1,2 @@
+Cd C:\Program Files\SmartBear\ReadyAPI-1.9.0\bin\
+cmd.exe /C testrunner.bat -a -J "-fC:\SoapUI Automation\Reports" "-RJUnit-Style HTML Report" "-EDefault environment" -I "C:\SoapUI Automation\REST-Sample-Project-soapui-project.xml"
